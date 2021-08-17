5088bbdb40f65b806cd0d097343495e139f41cd4
John <J.G.Lokerse@gmail.com>
2021-08-17 09:03:27 +0200

    Added Create-BulkAppRegs.ps1

---
+++ Create-BulkAppRegs.ps1
@@ -0,0 +1,23 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [string[]] $Owners,
+    [Parameter(Mandatory = $true)]
+    [String[]] $AppRegNames
+)
+
+foreach ($appReg in $AppRegNames) {
+    [string] $appRegId = az ad app create --display-name $appReg --query "appId"
+    Write-Host "Created appreg: $appReg"
+
+    foreach ($owner in $Owners) {
+        [string] $ownerId = az ad user show --id $owner --query "objectId"
+        az ad app owner add --id $appRegId --owner-object-id $ownerId
+        Write-Host "$owner added as owner"
+    }
+
+    az ad app owner remove --id $appRegId --owner-object-id $(az ad signed-in-user show --query "objectId")
+    Write-Host "Removed myself as owner"
+}
+
+# Example
+# .\Create-BulkAppRegs.ps1 -Owners "john.doe@something.nl", "jane.doe@something.nl" -AppRegNames "AppRegNameA", "AppRegNameP"
